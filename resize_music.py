5077baac960d7b06c18ad9c5f50f167602ce94da
Bernhard Hering <bernhard.hering@slub-dresden.de>
2020-12-04 12:27:55 +0100

    huge rewrite, no files are getting created in the source directory

--- resize_music.py
+++ resize_music.py
@@ -6,8 +6,6 @@ import argparse
 import traceback
 from multiprocessing import Pool,current_process,cpu_count
 from shutil import copyfile, which
-from mutagen.mp3 import MP3
-from mutagen.aac import AAC
 from uuid import uuid4
 # ----------------------------------------------------------------------------
 # "THE BEER-WARE LICENSE" (Revision 42):
@@ -33,122 +31,88 @@ for tool in ["faac",
 
 def change_ending(name, _format):
     spltfld = name.split('.')
-    spltfld[-1] = _format
+    if len(spltfld)>1:
+        spltfld[-1] = _format
+    else:
+        spltfld.append(_format)
     return '.'.join(spltfld)
 
 
-def encode(_format, _bitrate, src, tgt):
-    if _format == "mp3":
-        subprocess.call(("lame", "--quiet", "-b", str(_bitrate), src, tgt), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    elif _format == "aac":
-        subprocess.call(("faac", "-v0", "-b", str(_bitrate), "-o", tgt, src), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+def remove(path):
+    if os.path.exists(path):
+        os.remove(path)
 
+
+def transform(_format, _in, _out):
+    toDelete = []
+    try:
+        if _format == "mp3":  # decode
+            call_obj = ("mpg321", "-w", _out, _in)
+            toDelete.append(change_ending(_out, "wav"))
+        elif _format == "faad":  # decode
+            call_obj = ("faad", "-q", "-o", _out, _in)
+            toDelete.append(change_ending(_out, "wav"))
+        elif _format == "wma":  # decode
+            call_obj = ("ffmpeg", "-y", "-i", _in, _out)
+            toDelete.append(change_ending(_out, "wav"))
+        elif _format == "flac":  # decode
+            call_obj = ("flac", "-d", "-f", _in, "-o", _out)
+            toDelete.append(change_ending(_out, "wav"))
+        elif _format == "ogg":  # decode
+            call_obj = ("oggdec", _in)
+            toDelete.append(change_ending(_in, "wav"))
+        elif _format == "mpc":  # decode
+            call_obj = ("mpcdec", _in, _out)
+            toDelete.append(_out)
+        elif _format == "lame":  # encode
+            call_obj = ("lame", "--quiet", "-b", str(bitrate), _in, _out)
+        elif _format == "aac":  # encode
+            call_obj = ("faac", "-v0", "-b", str(bitrate), "-o", _out, _in)
+            toDelete.append(change_ending(_in, "wav"))
+        else:
+            raise ValueError("{} not supported".format(_format))
+        with open("{}-stdout.txt".format(current_process().name),"at") as sout, open("{}-stderr.txt".format(current_process().name),"at") as serr:
+            subprocess.call(call_obj, stdout=sout, stderr=serr)
+    except Exception as e:
+        with open("errors.txt", 'a') as f:
+            traceback.print_exc(file=f)
+    return toDelete
 
 def copy_id3(srcname, destname):
-    try:
+    if not encoding == "aac":
         src = mutagen.File(srcname, easy=True)
         dest = mutagen.File(destname, easy=True)
         for k in src:
-            try:
-                dest[k] = src[k]
-            except:
-                continue
+            dest[k] = src[k]
         dest.save()
-    except Exception as e:
-        with open("errors.txt", 'a') as f:
-            traceback.print_exc(file=f)
+    #except Exception as e:
+        #with open("errors.txt", 'a') as f:
+            #traceback.print_exc(file=f)
 
 
-def reencode_mp3(path):
+def reencode_generic2wav2target(path, codec):
+    toDelete = []
+    tmpfile = "/tmp/" + str(uuid4()) + ".wav"
+    target_file = change_ending(prefix + path, encoding)
+    toDelete + transform(codec, path, tmpfile)
+    toDelete + transform(encoding, tmpfile, target_file)
+    toDelete.append(tmpfile)
+    copy_id3(path, target_file)
+    for item in toDelete:
+        remove(item)
+
+
+def reencode_native(path, source_codec):
     try:
-        if encoding == "mp3":
-            f = MP3(path)
-            if bitrate < int(f.info.bitrate / 1000)+10:
-                encode(encoding, bitrate, path, prefix+path)
-            else:
-                copyfile(path, prefix+path)
-        elif encoding == "aac":
-            tmpfile = "/tmp/" + str(uuid4()) + ".wav"
-            target_file = change_ending(prefix + path, encoding)
-            subprocess.call(("mpg321", "-w", tmpfile, path),stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-            encode(encoding, bitrate, tmpfile, target_file)
-            copy_id3(path, target_file)
-            os.remove(tmpfile)
-            
-    except Exception as e:
-        with open("errors.txt", 'a') as f:
-            traceback.print_exc(file=f)
-
-
-def reencode_m4a(path):
-    try:
-        source_bitrate = int(AAC(path).info.bitrate / 1000)
-    except mutagen.aac.AACError:
+        source_bitrate = int(mutagen.File(path).info.bitrate / 1000)
+    except (mutagen.aac.AACError, mutagen.mp3.MP3Error):
         source_bitrate = 5000
-    try:
-        target_file = change_ending(prefix + path, encoding)
-        if encoding == "mp3" or source_bitrate > bitrate:
-            tmpfile = "/tmp/" + str(uuid4()) + ".wav"
-            subprocess.call(("faad", "-q", "-o", tmpfile, path),stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-            encode(encoding, bitrate, tmpfile, target_file)
-            copy_id3(path, target_file)
-            os.remove(tmpfile)
-        elif encoding == "aac" and source_bitrate <= bitrate+10:
-            copyfile(path, target_file)
-    except Exception as e:
-        with open("errors.txt", 'a') as f:
-            traceback.print_exc(file=f)
-
-
-def reencode_wma(path):
-    try:
-        tmpfile = "/tmp/" + str(uuid4()) + ".wav"
-        target_file = change_ending(prefix + path, encoding)
-        subprocess.call(("ffmpeg", "-y", "-i", path, tmpfile),stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        encode(encoding, bitrate, tmpfile, target_file)
-        copy_id3(path, target_file)
-        os.remove(tmpfile)
-    except Exception as e:
-        with open("errors.txt", 'a') as f:
-            traceback.print_exc(file=f)
-
-
-def reencode_flac(path):
-    try:
-        tmpfile = "/tmp/" + str(uuid4()) + ".wav"
-        target_file = change_ending(prefix + path, encoding)
-        subprocess.call(("flac", "-d", "-f", path, "-o", tmpfile),stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        encode(encoding, bitrate, tmpfile, target_file)
-        copy_id3(path, target_file)
-        os.remove(tmpfile)
-    except Exception as e:
-        with open("errors.txt", 'a') as f:
-           traceback.print_exc(file=f)
-
-def reencode_ogg(path):
-    try:
-        tmpfile = change_ending(path, "wav")
-        target_file = change_ending(prefix + path, encoding)
-        subprocess.call(("oggdec", path),stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        encode(encoding, bitrate, tmpfile, target_file)
-        copy_id3(path, target_file)
-        os.remove(tmpfile)
-    except Exception as e:
-        with open("errors.txt", 'a') as f:
-            traceback.print_exc(file=f)
-
-
-def reencode_mpc(path):
-    try:
-        tmpfile = "/tmp/" + str(uuid4()) + ".wav"
-        target_file = change_ending(prefix+path, encoding)
-        subprocess.call(("mpcdec", path, tmpfile),stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        encode(encoding, bitrate, tmpfile, target_file)
-        copy_id3(path, target_file)
-        os.remove(tmpfile)
-    except Exception as e:
-        with open("errors.txt",'a') as f:
-            traceback.print_exc(file=f)
+    if encoding == source_codec and source_bitrate <= bitrate+10:
+        copyfile(path, prefix+path)
+    elif encoding == source_codec and encoding == "mp3" and source_bitrate > bitrate+10:
+        transform("lame", path, prefix+path)
+    else:
+        reencode_generic2wav2target(path, source_codec)
 
 
 def init(p, b, e):
@@ -168,24 +132,27 @@ if __name__ == "__main__":
     parser.add_argument('-o', type=str, default="klein", help="Prefix for the Output-directory")
     parser.add_argument('-t', type=str, default="mp3", help="Target Encoding, support for MP3 and M4A. While MP3 is more available on end user devices, AAC offers better quality on lower bitrates.")
     parser.add_argument('-b', type=int, default=128, help="Target-Bitrate")
+    parser.add_argument('-debug', action='store_true', default=False, help="disable multiprocessing for debugging")
     args=parser.parse_args()
-    pool = Pool(args.w, initializer=init, initargs=(args.o, args.b, args.t))
+    if not args.debug:
+        pool = Pool(args.w, initializer=init, initargs=(args.o, args.b, args.t))
+    else:
+        init(args.o, args.b, args.t)
     for root, dirs, files in os.walk(args.i, topdown=False):
         if not os.path.isdir(args.o+root):
             os.makedirs(args.o+root)
         for name in files:
             ending = name.split(".")[-1].lower()
-            if ending == "mp3":
-                pool.apply_async(reencode_mp3, (os.path.join(root, name),))
-            elif ending == "flac":
-                pool.apply_async(reencode_flac, (os.path.join(root, name),))
-            elif ending == "ogg":
-                pool.apply_async(reencode_ogg, (os.path.join(root,name),))
-            elif ending == "wma":
-                pool.apply_async(reencode_wma, (os.path.join(root, name),))
-            elif ending == "mpc":
-                pool.apply_async(reencode_mpc, (os.path.join(root, name),))
-            elif ending in ["m4a", "aac"]:
-                pool.apply_async(reencode_m4a, (os.path.join(root, name),))
-    pool.close()
-    pool.join()
+            if ending in ["mpc", "wma", "ogg", "flac"]:
+                if not args.debug:
+                    pool.apply_async(reencode_generic2wav2target, (os.path.join(root, name), ending,))
+                else:
+                    reencode_generic2wav2target(os.path.join(root, name), ending)
+            elif ending in ["m4a", "aac", "mp3"]:
+                if not args.debug:
+                    pool.apply_async(reencode_native, (os.path.join(root, name), ending,))
+                else:
+                    reencode_native(os.path.join(root, name), ending)
+    if not args.debug:
+        pool.close()
+        pool.join()
